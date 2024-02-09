b1995cff10555b5323de8bafa5e4e2e16ac169f3
ChidhvilasTanay <mctanay@gmail.com>
2024-02-09 16:49:11 +0530

    message

--- main.go
+++ main.go
@@ -1,1 +1,18 @@
 package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	fileServer := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fileServer)
+	http.HandleFunc("/form", formHandler)
+	http.HandleFunc("/hello", helloHandler)
+
+	fmt.printf("started at 8080\n")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
+}
